f4ba3255b052758b920dd87caef1f89929492989
mnm9994u <xxx@xxx>
2013-04-16 15:22:47 +0800

    Status bar Auto-Hide [1/2] Add an option to auto-hide status bar when there are no notifications.
    PS 2 : Improved Logic
    
    Change-Id: I9f9fdb443f6ed981c1941716491cef757b42f8ba
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -386,6 +386,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mVolBtnMusicControls;
     boolean mIsLongPress;
 
+    boolean mHideStatusBar;
+
     private static final class PointerLocationInputEventReceiver extends InputEventReceiver {
         private final PointerLocationView mView;
 
@@ -674,6 +676,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.ON_SCREEN_BUTTONS_HEIGHT), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ON_SCREEN_BUTTONS_WIDTH), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HIDE_STATUSBAR), false, this);
 
             updateSettings();
         }
@@ -3548,6 +3552,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // and mTopIsFullscreen is that that mTopIsFullscreen is set only if the window
                 // has the FLAG_FULLSCREEN set.  Not sure if there is another way that to be the
                 // case though.
+                mHideStatusBar = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.HIDE_STATUSBAR, 0) == 1;
                 if (topIsFullscreen) {
                     if (DEBUG_LAYOUT) Log.v(TAG, "** HIDING status bar");
                     if (mStatusBar.hideLw(true)) {
