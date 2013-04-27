ea0afc8b6d161140a95d46268cd57d16d1f28941
sethyx <sethyx@gmail.com>
2013-04-27 20:52:22 +0800

    NFC Polling Mode (3/3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3727,6 +3727,13 @@ public final class Settings {
         */
         public static final String NOTIF_ALPHA = "notif_alpha";
 
+        /**
+         * NFC polling mode configuration key
+         *
+         * @hide
+         */
+        public static final String NFC_POLLING_MODE = "nfc_polling_mode";
+
     }
 
     /**
